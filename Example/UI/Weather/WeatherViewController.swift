8c37b8dfbb25b50661e00b71917db9d6cd8f3403
tanobe <stomp0630@icloud.com>
2021-11-12 16:48:28 +0900

    Changed NotificationCenter back to the previous one.

--- Example/UI/Weather/WeatherViewController.swift
+++ Example/UI/Weather/WeatherViewController.swift
@@ -29,12 +29,7 @@ class WeatherViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        NotificationCenter.default.addObserver(forName: UIApplication.didBecomeActiveNotification,
-                                               object: nil,
-                                               queue: nil) { [weak self] _ in
-            guard let self = self else {
-                return
-            }
+        NotificationCenter.default.addObserver(forName: UIApplication.didBecomeActiveNotification, object: nil, queue: nil) { [unowned self] notification in
             self.loadWeather()
         }
     }
